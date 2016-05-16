371f83b4d8c300bd059c02acd811de220613db35
Andrew Hinett <andrewhinett@gmail.com>
2016-05-15 22:04:00 -0700

    Ansible and CircleCI configuration

---
+++ bin/playbook.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ansible-playbook -i inf/${1}/hosts playbooks/default.yml
--- inf/master/dns.tf
+++ inf/master/dns.tf
@@ -2,6 +2,7 @@ resource "aws_route53_record" "a" {
    zone_id = "${var.zone_id}"
    name = "${var.zone_name}"
    type = "A"
+   ttl = "300"
    records = ["${aws_instance.master.public_ip}"]
 }
 
@@ -9,5 +10,6 @@ resource "aws_route53_record" "www" {
    zone_id = "${var.zone_id}"
    name = "www.${var.zone_name}"
    type = "CNAME"
+   ttl = "3600"
    records = ["${var.zone_name}"]
 }
