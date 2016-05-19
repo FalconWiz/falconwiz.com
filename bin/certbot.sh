cb74e2b804cb488dae65506633b7bc78804306cf
Andrew Hinett <andrewhinett@gmail.com>
2016-05-19 13:01:28 -0700

    Update certbot script with proper webroot path

--- bin/certbot.sh
+++ bin/certbot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
-./certbot-auto certonly -d falconwiz.com -d www.falconwiz.com --rsa-key-size 4096 --webroot --webroot-path /usr/share/nginx/html
+./certbot-auto certonly -d falconwiz.com -d www.falconwiz.com --rsa-key-size 4096 --webroot --webroot-path /var/www/falconwiz.com/
