42a7dbc5c9095ca5aff3609a3cec70562745a31e
Andrew Hinett <andrewhinett@gmail.com>
2016-06-23 18:06:49 -0400

    Changed site layout, will have to revisit styling issues. Closes #18 #17 #16 #13.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import NavBar from './NavBar'
-import Footer from './Footer'
-import VideoPlayer from './VideoPlayer'
-import 'bootstrap/dist/css/bootstrap.css'
 
-const falconwizCss = require('../css/falconwiz.css') // eslint-disable-line no-unused-vars
+import SiteWrapper from './SiteWrapper'
+import Header from './Header'
+import InnerCover from './InnerCover'
+import VideoPlayer from './VideoPlayer'
+import Footer from './Footer'
+
+// Bootstrap / Style things
+global.jQuery = require('jquery')
+global.Tether = require('tether')
+require('bootstrap')
+require('./../css/falconwiz.scss')
 
 class Main extends React.Component {
   render () {
     return (
-      <div>
-        <div>
-          <NavBar />
-        </div>
-        <div>
+      <SiteWrapper>
+        <Header />
+        <InnerCover>
           <VideoPlayer />
-        </div>
-        <div>
-          <Footer />
-        </div>
-      </div>
+        </InnerCover>
+        <Footer />
+      </SiteWrapper>
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var path = require('path')
 var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: [
@@ -16,8 +15,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'FalconWiz.com',
       template: 'src/index.html'
-    }),
-    new ExtractTextPlugin('styles/styles.css', {publicPath: '/styles/'})
+    })
   ],
   module: {
     loaders: [
@@ -33,8 +31,8 @@ module.exports = {
       },
       // CSS
       {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        test: /(\.css|\.scss)$/,
+        loaders: ['style', 'css', 'sass']
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
