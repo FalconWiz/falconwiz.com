239d3b0875791d1098c59a9af6df2b79a07f04d0
Andrew Hinett <andrewhinett@gmail.com>
2016-08-10 14:12:53 -0400

    Added line break in bio sentence

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,7 +8,7 @@ const Bio = Component({
           Who am I?
         </h2>
         <p className="text-xs-center">
-          Hello! I'm Andrew, just a regular human who loves to learn about (and build things with) new and exciting technologies!
+          Hello! I'm Andrew, just a regular human who loves to learn about <br/> (and build things with) new and exciting technologies!
         </p>
       </div>
     )
