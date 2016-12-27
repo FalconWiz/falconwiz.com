a1059a3172dd885cae5d55aed906346021981147
Andrew Hinett <andrewhinett@gmail.com>
2016-12-26 20:41:50 -0500

    Added remote-exec provisioner so Terraform will update and install python, which is needed for Ansible to work

--- inf/master/server.tf
+++ inf/master/server.tf
@@ -6,4 +6,15 @@ resource "aws_instance" "master" {
   }
   key_name = "${var.pubkey}"
   vpc_security_group_ids = ["${aws_security_group.allow_web_traffic.id}"]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install --no-install-recommends --assume-yes python-apt"
+    ]
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+    }
+  }
 }
